67d6eab80345291d8b71ecff9c329392c7cc0b70
Eric McNiece <emcniece@gmail.com>
2018-08-27 21:50:12 -0700

    Working iOS methods

--- src/ios/RSSI.swift
+++ src/ios/RSSI.swift
@@ -1,18 +1,18 @@
 /**
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2018 Eric McNiece <emcniece@gmail.com>
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,47 +20,143 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
-*/
+ */
 
 import Foundation
 
+/*
+ * UIDevice Extension
+ * Detects iPhoneX devices.
+ * Usage: `if UIDevice.isIphoneX {}`
+ * https://stackoverflow.com/a/47566231/943540
+ */
+extension UIDevice {
+    static var isIphoneX: Bool {
+        var modelIdentifier = ""
+        if isSimulator {
+            modelIdentifier = ProcessInfo.processInfo.environment["SIMULATOR_MODEL_IDENTIFIER"] ?? ""
+        } else {
+            var size = 0
+            sysctlbyname("hw.machine", nil, &size, nil, 0)
+            var machine = [CChar](repeating: 0, count: size)
+            sysctlbyname("hw.machine", &machine, &size, nil, 0)
+            modelIdentifier = String(cString: machine)
+        }
+        
+        return modelIdentifier == "iPhone10,3" || modelIdentifier == "iPhone10,6"
+    }
+    
+    static var isSimulator: Bool {
+        return TARGET_OS_SIMULATOR != 0
+    }
+}
+
 @objc(RSSI) public class RSSI : CDVPlugin  {
-
-    override public func pluginInitialize() {
-    }
-
-    override public func onAppTerminate() {
-    }
-
-    
     public func read(_ command: CDVInvokedUrlCommand) {
-
-        let hostname = "test" //Hostname.get() as String
-
+        var rssi: Any
+        var bars: Int!
+        var isIPhoneX: Bool! = false
+        
+        if UIDevice.isIphoneX {
+            isIPhoneX = true
+            rssi = NSNull() // No method for obtaining RSSI on iPhoneX
+            bars = self.getXWifiBars()
+        } else {
+            rssi = self.getNormalWifiRSSI() as Any
+            bars = self.getNormalWifiBars()
+        }
+        
         #if DEBUG
-            print("RSSI: hostname \(hostname)")
+            print("RSSI: rssi: \(rssi), bars: \(bars), isIPhoneX: \(isIPhoneX)")
         #endif
 
-        let pluginResult = CDVPluginResult(status:CDVCommandStatus_OK, messageAs: hostname)
-        self.commandDelegate?.send(pluginResult, callbackId: command.callbackId)
-    }
-    
-
-    /*
-    public func read(_ command: CDVInvokedUrlCommand) {
-
-        #if DEBUG
-            print("RSSI: read")
-        #endif
-
-        let hostname = Hostname.get() as String
         let message: NSDictionary = NSDictionary(
-            objects: [hostname, false, false, false],
-            forKeys: ["hostname" as NSCopying, "connection" as NSCopying, "interfaces" as NSCopying, "dhcp" as NSCopying]
+            objects: [rssi, bars, isIPhoneX],
+            forKeys: ["rssi" as NSCopying, "bars" as NSCopying, "isIPhoneX" as NSCopying]
         )
-
+        
         let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: message as! [AnyHashable: Any])
         self.commandDelegate?.send(pluginResult, callbackId: command.callbackId)
     }
-    */
+    
+    private func getNormalWifiRSSI() -> Int? {
+        let app = UIApplication.shared
+        var rssi: Int?
+        
+        let exception = tryBlock {
+            guard let statusBar = app.value(forKey: "statusBar") as? UIView else { return }
+            if let statusBarMorden = NSClassFromString("UIStatusBar_Modern"), statusBar .isKind(of: statusBarMorden) { return }
+            guard let foregroundView = statusBar.value(forKey: "foregroundView") as? UIView else { return  }
+            
+            for view in foregroundView.subviews {
+                if let statusBarDataNetworkItemView = NSClassFromString("UIStatusBarDataNetworkItemView"), view .isKind(of: statusBarDataNetworkItemView) {
+                    if let val = view.value(forKey: "wifiStrengthRaw") as? Int {
+                        rssi = val
+                        break
+                    }
+                }
+            }
+        }
+        
+        if let exception = exception {
+            print("getWiFiRSSI exception: \(exception)")
+        }
+        
+        return rssi
+    }
+    
+    private func getNormalWifiBars() -> Int? {
+        let app = UIApplication.shared
+        var bars: Int?
+        
+        let exception = tryBlock {
+            guard let statusBar = app.value(forKey: "statusBar") as? UIView else { return }
+            if let statusBarMorden = NSClassFromString("UIStatusBar_Modern"), statusBar .isKind(of: statusBarMorden) { return }
+            guard let foregroundView = statusBar.value(forKey: "foregroundView") as? UIView else { return  }
+            
+            for view in foregroundView.subviews {
+                if let statusBarDataNetworkItemView = NSClassFromString("UIStatusBarDataNetworkItemView"), view .isKind(of: statusBarDataNetworkItemView) {
+                    if let val = view.value(forKey: "wifiStrengthBars") as? Int {
+                        bars = val
+                        break
+                    }
+                }
+            }
+        }
+        
+        if let exception = exception {
+            print("getNormalWifiBars exception: \(exception)")
+        }
+        
+        return bars
+    }
+    
+    private func getXWifiBars() -> Int? {
+        let app = UIApplication.shared
+        var bars: Int?
+        
+        let exception = tryBlock {
+            guard let containerBar = app.value(forKey: "statusBar") as? UIView else { return }
+            guard let statusBarMorden = NSClassFromString("UIStatusBar_Modern"), containerBar .isKind(of: statusBarMorden) else { return }
+            guard let statusBar = containerBar.value(forKey: "statusBar") as? UIView else { return }
+            guard let foregroundView = statusBar.value(forKey: "foregroundView") as? UIView else { return }
+            
+            for view in foregroundView.subviews {
+                for v in view.subviews {
+                    if let statusBarWifiSignalView = NSClassFromString("_UIStatusBarWifiSignalView"), v .isKind(of: statusBarWifiSignalView) {
+                        if let val = v.value(forKey: "numberOfActiveBars") as? Int {
+                            bars = val
+                            break
+                        }
+                    }
+                }
+            }
+        }
+        
+        if let exception = exception {
+            print("getXWifiBars exception: \(exception)")
+        }
+        
+        return bars
+    }
 }
